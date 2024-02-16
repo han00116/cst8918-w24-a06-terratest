325cadfabde4d893661c3db4843cb372555dfcef
Andrew Han <han00116@algonquinlive.com>
2024-02-16 12:40:54 -0500

    adding the 2 tests required

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -9,30 +9,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type VMImage struct {
-	Publisher string
-	Offer     string
-	SKU       string
-	Version   string
-}
-
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
 var subscriptionID string = "f297c7ae-463c-406d-9c62-7eb1d616382a"
 
-func GetVirtualMachineImageE(vmName string, resGroupName string, subscriptionID string) (VMImage, error) {
-	// Implement this function to interact with Azure and retrieve VM image information
-	return VMImage{}, nil
-}
-
-func GetVirtualMachineNicsE(vmName string, resGroupName string, subscriptionID string) ([]string, error) {
-	// Implement this function to interact with Azure and retrieve VM NIC information
-	return []string{}, nil
-}
-
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
-		TerraformDir: "../", // Path to Terraform code
+		// The path to where our Terraform code is located
+		TerraformDir: "../",
+		// Override the default terraform variables
 		Vars: map[string]interface{}{
 			"labelPrefix": "han00116",
 		},
@@ -40,20 +25,27 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 
 	defer terraform.Destroy(t, terraformOptions)
 
+	// Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
 	terraform.InitAndApply(t, terraformOptions)
 
+	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 
+	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+}
 
-	// Get VM image
-	vmImage, err := GetVirtualMachineImageE(vmName, resourceGroupName, subscriptionID)
+func GetVirtualMachineImage(t testing.T, vmName string, resGroupName string, subscriptionID string) VMImage {
+	vmImage, err := GetVirtualMachineImageE(vmName, resGroupName, subscriptionID)
 	require.NoError(t, err)
-	assert.NotNil(t, vmImage)
 
-	// Get VM NICs
-	nicList, err := GetVirtualMachineNicsE(vmName, resourceGroupName, subscriptionID)
+	return vmImage
+}
+
+func GetVirtualMachineNics(t testing.T, vmName string, resGroupName string, subscriptionID string) []string {
+	nicList, err := GetVirtualMachineNicsE(vmName, resGroupName, subscriptionID)
 	require.NoError(t, err)
-	assert.NotNil(t, nicList)
+
+	return nicList
 }
